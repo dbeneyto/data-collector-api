8b35a46b8f1fb5303a15d7591e0d9ca4f8545f8c
danfaizer <dbeneyto@gmail.com>
2013-06-01 19:11:00 +0200

    Sync

--- api/station.php
+++ api/station.php
@@ -15,39 +15,54 @@ function bikesystem() {
 * i.e: http:///bdc.labobila.com/api/system/bicing/station
 * Get @all stations from {system-name}
 */
-$app->get('/api/system/:bikesystem/station','station')->name('bikesystemname');
-function station($bikesystemname) {
-try {
-  // open connection to MongoDB server
-  $conn = new Mongo('localhost');
+$app->get('/api/system/:bikesystem/station','stations')->name('bikesystemname');
+function stations($bikesystemname) {
+	try {
+		$conn = new Mongo(DB_SERVER_IP);
+		$db = $conn->$bikesystemname;
+		$collection = $db->station;
+		$cursor = $collection->find();
 
-  // access database
-  $db = $conn->bicing;
+		$stations=array();
+		foreach ($cursor as $obj) {
+			$station_info=array($obj['id'],$obj['cleaname'],$obj['location']);
+			array_push($stations,json_encode($station_info));
+		}
+		echo json_encode($stations);
 
-  // access collection
-  $collection = $db->station;
-
-  // execute query
-  // retrieve all documents
-  $cursor = $collection->find()->limit(400);
-
-  // iterate through the result set
-  // print each document
-  $stations=array();
-  foreach ($cursor as $obj) {
-        $station_info=array($obj['id'],$obj['cleaname'],$obj['location']);
-	array_push($stations,json_encode($station_info));
-  }
-  echo json_encode($stations);
-
-  // disconnect from server
-  $conn->close();
-} catch (MongoConnectionException $e) {
-  die('Error connecting to MongoDB server');
-} catch (MongoException $e) {
-  die('Error: ' . $e->getMessage());
+		// disconnect from server
+		$conn->close();
+	} catch (MongoConnectionException $e) {
+		die('Error connecting to MongoDB server');
+	} catch (MongoException $e) {
+		die('Error: ' . $e->getMessage());
+	}
 }
 
+/*
+ * /api/system/{system-name}/station/{station-id}
+* i.e: http:///bdc.labobila.com/api/system/bicing/station/1
+* Get @all data from {system-name} and station {station-id}
+*/
+$app->get('/api/system/:bikesystem/station/:id','station')->name('bikesystemname','idstation');
+function station($bikesystemname,$idstation) {
+	try {
+		$conn = new Mongo(DB_SERVER_IP);
+		$db = $conn->$bikesystemname;
+		$collection = $db->station;
+		$collectionQuery=array('id'=>$idstation);
+		$cursor = $collection->find($collectionQuery);
+
+		foreach ($cursor as $obj) {
+			echo json_encode($obj);
+		}
+		// disconnect from server
+		$conn->close();
+	} catch (MongoConnectionException $e) {
+		die('Error connecting to MongoDB server');
+	} catch (MongoException $e) {
+		die('Error: ' . $e->getMessage());
+	}
 }
 
 
--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?
 /**
+ * Require MongoDB connection parameters
+ */
+require_once('mongodb.cfg');
+
+/**
  * Require the Slim Framework
  */
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
-$app = new \Slim\Slim();	
+$app = new \Slim\Slim();
 
 /*
  *  Station resource
- */
+*/
 include('./api/station.php');
 
 $app->run();
